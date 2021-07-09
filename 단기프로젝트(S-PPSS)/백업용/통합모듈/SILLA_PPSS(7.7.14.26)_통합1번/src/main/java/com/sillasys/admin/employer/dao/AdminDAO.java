6d524551a682bd94867a06bc6ab6be5cde27e7d3
ssom94091 <63699194+ssom94091@users.noreply.github.com>
2021-07-09 10:13:01 +0900

    영민 ver.1

---
+++ 단기프로젝트(S-PPSS)/백업용/통합모듈/SILLA_PPSS(7.7.14.26)_통합1번/src/main/java/com/sillasys/admin/employer/dao/AdminDAO.java
@@ -0,0 +1,17 @@
+package com.sillasys.admin.employer.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.sillasys.admin.employer.vo.AdminVO;
+
+import egovframework.rte.psl.dataaccess.EgovAbstractMapper;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+@Repository("adminDAO")
+public class AdminDAO extends EgovAbstractMapper{
+	
+	public EgovMap selectAdmin(AdminVO param) throws Exception{
+		return selectOne("adminDAO.selectAdmin", param);
+	}
+
+}
