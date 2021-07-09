5596dcd5405cf67ebd5b4a0de8c5a31be665a1b0
jspsibal <86281622+jspsibal@users.noreply.github.com>
2021-07-09 09:10:12 +0900

    pr

---
+++ 단기프로젝트(S-PPSS)/백업용/류지원/SILLA_PPSS(7.7.14.26)_지원1번/src/main/java/com/sillasys/user/employee/service/UserService.java
@@ -0,0 +1,11 @@
+package com.sillasys.user.employee.service;
+
+import com.sillasys.user.employee.vo.UserVO;
+
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+public interface UserService {
+	public void doJoin(UserVO param) throws Exception;
+	
+	public EgovMap selectLoginUser(UserVO param) throws Exception;
+}
