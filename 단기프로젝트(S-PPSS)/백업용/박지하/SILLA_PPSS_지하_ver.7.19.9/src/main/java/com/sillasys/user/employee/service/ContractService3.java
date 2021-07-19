98c13c41a31cc1dceb8190d1d43b17e53dcedcd2
bgpjh <bgpjh97@gmail.com>
2021-07-19 09:09:05 +0900

    pr

---
+++ 단기프로젝트(S-PPSS)/백업용/박지하/SILLA_PPSS_지하_ver.7.19.9/src/main/java/com/sillasys/user/employee/service/ContractService3.java
@@ -0,0 +1,7 @@
+package com.sillasys.user.employee.service;
+
+import com.sillasys.user.employee.vo.ContractVO3;
+
+public interface ContractService3 {
+   public void trmTemplate3(ContractVO3 param) throws Exception;
+}
---
+++ 단기프로젝트(S-PPSS)/백업용/박지하/SILLA_PPSS_지하_ver.7.19.9/src/main/java/com/sillasys/user/employee/vo/ContractVO3.java
@@ -0,0 +1,18 @@
+package com.sillasys.user.employee.vo;
+
+public class ContractVO3 {
+   private String frm;
+
+   public String getFrm() {
+      return frm;
+   }
+
+   public void setFrm(String frm) {
+      this.frm = frm;
+   }
+
+   @Override
+   public String toString() {
+      return "ContractVO3 [frm=" + frm + "]";
+   }      
+}
