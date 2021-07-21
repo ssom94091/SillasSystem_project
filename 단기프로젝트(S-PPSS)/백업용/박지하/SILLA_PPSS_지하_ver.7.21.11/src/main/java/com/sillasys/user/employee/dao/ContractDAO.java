4d09be8ff7727e61bc93315348dcc210c0eaaaf4
bgpjh <bgpjh97@gmail.com>
2021-07-21 11:33:10 +0900

    pr

---
+++ 단기프로젝트(S-PPSS)/백업용/박지하/SILLA_PPSS_지하_ver.7.21.11/src/main/java/com/sillasys/user/employee/dao/ContractDAO.java
@@ -0,0 +1,53 @@
+package com.sillasys.user.employee.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.sillasys.user.employee.vo.ContractVO;
+import com.sillasys.user.employee.vo.ContractWorkTimeVO;
+import com.sillasys.user.employee.vo.ContractEndTimeVO;
+import com.sillasys.user.employee.vo.ContractBenefitVO;
+
+import egovframework.rte.psl.dataaccess.EgovAbstractMapper;
+
+@Repository("ContractDAO")
+public class ContractDAO extends EgovAbstractMapper{
+	public void trmTemplate1(ContractVO param) throws Exception{
+		 insert("ContractDAO.trmTemplate1", param);
+		
+	}
+	
+	public void trmTemplate2(ContractVO param) throws Exception{
+		 insert("ContractDAO.trmTemplate2", param);
+	}
+	
+	 public void trmTemplate3(ContractVO param) throws Exception{
+	       insert("ContractDAO.trmTemplate3", param);
+	   }
+	   
+	   public void trmTemplate4(ContractVO param) throws Exception{
+	       insert("ContractDAO.trmTemplate4", param);
+	   }
+
+	
+	public void trmTemplateWork(ContractVO param) throws Exception{
+		 insert("ContractDAO.trmTemplateWork", param);
+		
+	}
+	
+	public void trmTemplateStWork(ContractVO param) throws Exception{
+	       insert("ContractDAO.trmTemplateStWork", param);
+	      
+	   }
+
+	
+	public void trmTemplateEnd(ContractVO param) throws Exception{
+		 insert("ContractDAO.trmTemplateEnd", param);
+		
+	}
+	
+	public void trmTemplateBenefit(ContractVO param) throws Exception{
+		 insert("ContractDAO.trmTemplateBenefit", param);
+		
+	}
+	
+}
